5b1be331faaf50f43e5ddfe957b5aa9cfa808282
volodya52 <157564207+volodya52@users.noreply.github.com>
2024-10-12 12:38:36 +0500

    Add files via upload

--- snake1.cpp
+++ snake1.cpp
@@ -1,80 +1,159 @@
-﻿// snake1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿using namespace std;
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
-#include <random>
-int width=20, height=10;
-struct SnakeNode 
-{
+int width = 20;
+int height = 10;
+int foodX;
+int foodY;
+int score;
+enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
+Direction dir;
+struct Snake {
     int x, y;
+    Snake* next;
 };
-
-
-
-void Draw(int headx, int heady,int foodx,int foody)
-{
-    system("cls");
-    bool snake = false;
-    for (int i = 0; i < width; i++) {
-        std::cout << "#";
-    }
-    std::cout << std::endl;
-    for (int x = 1; x < height - 1; x++) {
-        std::cout << "#";
-        for (int y = 1; y < width - 1; y++) {
-            if (y == headx && x == heady) {
-                std::cout << "0";
-                snake = true;
-                
-            }
-            else if(y==foodx&&x==foody) {
-                std::cout << "o";
-            }
-            else {
-                std::cout << " ";
-            }
+void input();
+void FoodGenerate(Snake* head);
+void Draw(Snake* head);
+bool CheckLoose(Snake* head);
+void Moving(Snake*& head, Snake*& tail);
+bool FoodCheck(Snake* head);
+void input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+        case 'a': dir = LEFT; break;
+        case 'd': dir = RIGHT; break;
+        case 'w': dir = UP; break;
+        case 's': dir = DOWN; break;
+        case '0': dir = STOP; break;
         }
-        std::cout << "#" << std::endl;
-    }
-    for (int i = 0; i < width; i++) {
-        std::cout << "#";
-    }
-    std::cout << std::endl;
-}
-void CheckEat() {
-
-}
-void CheckLoose() {
-
-}
-int main()
-{
-    int score = 0;
-    bool gameover=false;
-    int headx, heady,foodx,foody;
-    headx = width / 2;
-    heady = height / 2;
-    foodx = std::rand() % width;
-    foody = std::rand() % height;
-    std::cout << foodx<<std::endl;
-    std::cout << foody<<std::endl;
-    std::cout << "Welcome to the game - Snake!\n";
-    std::cout << "Control:\nW-up\nA-left\nD-right\nS-down\n"; 
-    while (gameover==false) {
-        Draw(headx,heady,foodx,foody);
-        Sleep(10);
     }
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void FoodGenerate(Snake* head) {
+    bool foodOnSnake;
+    do {
+        foodX = rand() % width;
+        foodY = rand() % height;
+        foodOnSnake = false;
+        Snake* temp = head;
+        while (temp != nullptr) {
+            if (temp->x == foodX && temp->y == foodY) {
+                foodOnSnake = true;
+                break;
+            }
+            temp = temp->next;
+        }
+    } while (foodOnSnake);
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+void Draw(Snake* head) {
+    system("cls");
+    for (int i = 0; i < width + 2; i++) cout << "#";
+    cout << endl;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0) cout << "#";
+            bool body = false;
+            Snake* temp = head;
+            while (temp != nullptr) {
+                if (temp->x == j && temp->y == i) {
+                    cout << "O";
+                    body = true;
+                }
+                temp = temp->next;
+            }
+            if (!body) {
+                if (foodX == j && foodY == i) cout << "o";
+                else cout << " ";
+            }
+
+            if (j == width - 1) cout << "#";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < width + 2; i++) cout << "#";
+    cout << endl;
+    cout << "Score: " << score << endl;
+}
+
+bool CheckLoose(Snake* head) {
+    if (head->x < 0 || head->x >= width || head->y < 0 || head->y >= height)
+        return true;
+
+    Snake* temp = head->next;
+    while (temp != nullptr) {
+        if (temp->x == head->x && temp->y == head->y)
+            return true;
+        temp = temp->next;
+    }
+
+    return false;
+}
+
+void Moving(Snake*& head, Snake*& tail) {
+    int prevX = head->x;
+    int prevY = head->y;
+    int tempX, tempY;
+
+    switch (dir) {
+    case LEFT: head->x--; break;
+    case RIGHT: head->x++; break;
+    case UP: head->y--; break;
+    case DOWN: head->y++; break;
+    default: break;
+    }
+
+    if (FoodCheck(head)) {
+        Snake* newSnake = new Snake();
+        newSnake->x = prevX;
+        newSnake->y = prevY;
+        newSnake->next = nullptr;
+        tail->next = newSnake;
+        tail = newSnake;
+        FoodGenerate(head);
+    }
+    else {
+        Snake* temp = head->next;
+        while (temp != nullptr) {
+            tempX = temp->x;
+            tempY = temp->y;
+            temp->x = prevX;
+            temp->y = prevY;
+            prevX = tempX;
+            prevY = tempY;
+            temp = temp->next;
+        }
+    }
+}
+
+bool FoodCheck(Snake* head) {
+    if (head->x == foodX && head->y == foodY) {
+        score += 10;
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    Snake* head = new Snake();
+    head->x = width / 2;
+    head->y = height / 2;
+    head->next = nullptr;
+    Snake* tail = head;
+    score = 0;
+    dir = RIGHT;
+    FoodGenerate(head);
+    while (dir != STOP) {
+        Draw(head);
+        input();
+        Moving(head, tail);
+        if (CheckLoose(head)) {
+            cout << "Game over!" << endl;           
+            break;
+        }
+        Sleep(100);
+    }    
+}
+
